2931eacf1028a5427839787c8bab518d6d0acdf7
vieiramanda11 <amanda_vieira@outlook.com>
2019-08-15 14:59:12 -0300

    fixing the logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,15 +1,14 @@
-#require_relative '../lib/board.rb'
+require_relative '../lib/board.rb'
 require_relative '../lib/player.rb'
 require_relative '../lib/game.rb'
 
 game = Game.new
-#board = Board.new
+board = Board.new
 puts 'Welcome to Tic Tac Toe game'
-game.draw_board
+board.draw_board
 until game.player_wins? 
-  game.draw_legend
   game.player_move
-  game.draw_board
+  board.draw_board
   game.player_wins?
   game.which_player?
 end
