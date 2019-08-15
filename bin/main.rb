abd3dab441a00ac27b5c663f5f306ff7eb88399e
vieiramanda11 <amanda_vieira@outlook.com>
2019-08-15 13:46:17 -0300

    fixing the input

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,15 @@
-require_relative '../lib/board.rb'
+#require_relative '../lib/board.rb'
 require_relative '../lib/player.rb'
 require_relative '../lib/game.rb'
 
+game = Game.new
+#board = Board.new
+puts 'Welcome to Tic Tac Toe game'
+game.draw_board
+until game.player_wins? 
+  game.draw_legend
+  game.player_move
+  game.draw_board
+  game.player_wins?
+  game.which_player?
+end
