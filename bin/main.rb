77ec43ab07f2259afca2797847fb22f8192a1bdb
adebola <onedebos@gmail.com>
2019-08-15 20:26:06 +0100

    fix player wins

--- bin/main.rb
+++ bin/main.rb
@@ -1,14 +1,13 @@
-require_relative '../lib/board.rb'
 require_relative '../lib/player.rb'
 require_relative '../lib/game.rb'
 
 game = Game.new
-board = Board.new
+
 puts 'Welcome to Tic Tac Toe game'
-board.draw_board
+game.draw_board
 until game.player_wins? 
   game.player_move
-  board.draw_board
+  game.draw_board
   game.player_wins?
   game.which_player?
 end
