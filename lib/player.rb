fb15b5b337b533c7f2877fbfa7cd6f8648dcad8e
adebola <onedebos@gmail.com>
2019-08-17 14:59:36 +0100

    move puts

--- lib/player.rb
+++ lib/player.rb
@@ -10,15 +10,4 @@ class Player
   end
 end
 
-module Drawing
-  
-  def draw_board
-    puts "\n"
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]}"
-    puts '-----------'
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]}"
-    puts '-----------'
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]}"
-  end
-  
-end
+
